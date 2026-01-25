8507967a73ec2d6d86ec83722f138a8773ad24fe
River Stanley <river.stanley173@gmail.com>
2026-01-24 23:18:41 -0500

    Filter pending posts to not-okayed (#4)
    * oops forgot a piece
    
    * Update version

--- mathnews-core/mathnews-core.php
+++ mathnews-core/mathnews-core.php
@@ -16,7 +16,7 @@
  * Plugin Name:       mathNEWS Core
  * Plugin URI:        mathnews.uwaterloo.ca
  * Description:       Revamp article submission workflow
- * Version:           1.4.0-alpha12
+ * Version:           1.4.0-alpha13
  * Author:            mathNEWS Editors
  * Author URI:        All licensing queries should be directed to mathnews@gmail.com
  * License:           AGPL-3.0
@@ -38,7 +38,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  */
 const PLUGIN_NAME = 'mathnews-core';
-const VERSION = '1.4.0-alpha12';
+const VERSION = '1.4.0-alpha13';
 const DB_VERSION = 1;
 
 define('MATHNEWS_CORE_VERSION', VERSION);  // legacy, deprecated
